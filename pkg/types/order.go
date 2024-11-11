bd58d5c4102babc0c489fa9749eb0e58e2f7bb26
pcxyz <pcxyz@nolook.pass>
2024-11-11 15:33:13 +0800

    feat: enhanve submit order types

--- pkg/types/order.go
+++ pkg/types/order.go
@@ -141,6 +141,7 @@ type SubmitOrder struct {
 	AveragePrice fixedpoint.Value `json:"averagePrice,omitempty"`
 
 	StopPrice fixedpoint.Value `json:"stopPrice,omitempty" db:"stop_price"`
+	TakePrice fixedpoint.Value `json:"takePrice,omitempty" db:"take_price"`
 
 	Market Market `json:"-" db:"-"`
 
